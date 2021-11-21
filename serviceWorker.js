40ea8bc0f9e1fdc78d4afeca72bc8e3f48061246
ProdPusherBot <dontemail@me.com>
2021-11-21 11:24:04 +0000

    New Update

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
-var cacheName = 'cache-v1'
+const cacheName = 'cache-v1'
 
-var filesToCache = [
+const filesToCache = [
   '/index.html',
   '/',
   '/dist/assets/index.ff80710c.js',
@@ -11,64 +11,65 @@ var filesToCache = [
   '/icons8-tick-box.svg',
 ]
 
-//Adding 'install' event listener
-self.addEventListener('install', function (event) {
+// Adding 'install' event listener
+self.addEventListener('install', (event) => {
   console.log('Event: Install')
 
   event.waitUntil(
-    //Open the cache
+    // Open the cache
     caches
       .open(cacheName)
-      .then(function (cache) {
-        //Adding the files to cache
-        return cache.addAll(filesToCache).then(function () {
+      .then((cache) => {
+        // Adding the files to cache
+        return cache.addAll(filesToCache).then(() => {
           console.log('All files are cached.')
-          return self.skipWaiting() //To forces the waiting service worker to become the active service worker
+          return self.skipWaiting() // To forces the waiting service worker to become the active service worker
         })
       })
-      .catch(function (err) {
+      .catch((err) => {
         console.log(err)
       }),
   )
 })
 
-//Adding 'activate' event listener
-self.addEventListener('activate', function (event) {
+// Adding 'activate' event listener
+self.addEventListener('activate', (event) => {
   console.log('Event: Activate')
 
   event.waitUntil(
-    caches.keys().then(function (cacheNames) {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(function (cache) {
+        // eslint-disable-next-line array-callback-return
+        cacheNames.map((cache) => {
           if (cache !== cacheName) {
-            //cacheName = 'cache-v1'
-            return caches.delete(cache) //Deleting the cache
+            // cacheName = 'cache-v1'
+            return caches.delete(cache) // Deleting the cache
           }
         }),
       )
     }),
   )
 
-  return self.clients.claim() //To activate this SW immediately without waiting.
+  return self.clients.claim() // To activate this SW immediately without waiting.
 })
 
-//Adding 'fetch' event listener
-self.addEventListener('fetch', function (event) {
-  var request = event.request
+// Adding 'fetch' event listener
+self.addEventListener('fetch', (event) => {
+  const request = event.request
 
-  //Tell the browser to wait for network request and respond with below
+  // Tell the browser to wait for network request and respond with below
   event.respondWith(
-    //If request is already in cache, return it
-    caches.match(request).then(function (response) {
+    // If request is already in cache, return it
+    caches.match(request).then((response) => {
       if (response) {
         return response
       }
 
-      //else add the request to cache and return the response
-      return fetch(request).then(function (response) {
-        var responseToCache = response.clone() //Cloning the response stream in order to add it to cache
-        caches.open(cacheName).then(function (cache) {
-          cache.put(request, responseToCache) //Adding to cache
+      // else add the request to cache and return the response
+      return fetch(request).then((response) => {
+        const responseToCache = response.clone() // Cloning the response stream in order to add it to cache
+        caches.open(cacheName).then((cache) => {
+          cache.put(request, responseToCache) // Adding to cache
         })
         return response
       })
